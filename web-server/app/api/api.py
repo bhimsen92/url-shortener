b67839638c6e7a28a2bfd8fd1a09b859ff47c262
bhimsen92 <bhimsen.pes@gmail.com>
2020-06-27 23:00:49 +0200

    added api endpoints.

--- web-server/app/api/api.py
+++ web-server/app/api/api.py
@@ -1,18 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, redirect
+from werkzeug.exceptions import BadRequest
+from snowflake.client import SnowFlakeClient
+from models import Urls
 
 api_blueprint = Blueprint("api_blueprint", __name__)
 
 
 @api_blueprint.route("/<url_id>")
 def redirect(url_id):
-    pass
+    try:
+        result = Urls.get_original_url(url_id=url_id)
+        if result:
+            return redirect(result["original_url"], code=302)
+        raise Exception("url_id not found in db. It is probably expired." % url_id)
+    except Exception as e:
+        return jsonify({
+            "message": str(e)
+        })
 
 
 @api_blueprint.route("/<url_id>/<sub_url>")
 def redirect_redirect(url_id, sub_url):
-    pass
+    try:
+        result = Urls.get_original_url(url_id=url_id)
+        if result:
+            url = "%s/%s" % (result["original_url"], sub_url)
+            return redirect(url, code=302)
+        raise Exception("url_id not found in db. It is probably expired." % url_id)
+    except Exception as e:
+        return jsonify({
+            "message": str(e)
+        })
 
 
-@api_blueprint.route("/shorten")
+@api_blueprint.route("/shorten", methods=["POST"])
 def shorten_url():
-    pass
+    try:
+        payload = request.get_json()
+        original_url = payload.get("original_url", None)
+        if not original_url:
+            raise BadRequest("please provide url to shorten: %s" % original_url)
+        url_id = payload.get("custom_id", None)
+        if not url_id:
+            url_id = SnowFlakeClient.get_unique_url_id()
+        Urls.store_url(original_url=original_url, url_id=url_id)
+    except BadRequest as e:
+        return jsonify({
+            "message": e.message
+        })
+    except Exception as e:
+        return jsonify({
+            "message": "Service seems to be down. Please try again later."
+        })
--- web-server/models.py
+++ web-server/models.py
@@ -2,20 +2,19 @@ from db import DBMixin
 
 
 class Urls(DBMixin):
-    def get_original_url(self, url_id):
+    @classmethod
+    def get_original_url(cls, url_id):
         query = "select original_url, expires_at from urls where url_id = %s"
-        results = self.get(query, (url_id,))
+        results = cls.get(query, (url_id,))
         rows = [
             {"original_url": result.original_url, "expires_at": result.expires_at}
             for result in results
         ]
-        assert len(rows) == 1
+        assert len(rows) <= 1
         return rows
 
-    def store_url(self, original_url, url_id, created_by=None):
-        query = """
-        insert into urls (original_url, url_id, created_by)
-        values (%s, %s, %s)
-      """
-        results = self.post(query, (original_url, url_id, created_by,))
+    @classmethod
+    def store_url(cls, original_url, url_id, created_by=None):
+        query = """insert into urls (original_url, url_id, created_by) values (%s, %s, %s)"""
+        results = cls.post(query, (original_url, url_id, created_by,))
         return results
